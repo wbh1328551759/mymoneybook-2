efb0098b2615e431dcbc31bee339cec97acafd71
wbh1328551759 <1328551759@qq.com>
2020-07-01 22:12:12 +0800

    添加 SVG　文件

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,22 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
-  border:1px solid red;
   height: 100vh;
   display: flex;
   flex-direction: column;
+  font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
 `;
 const Main = styled.div`
-  border:1px solid black;
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.nav`
-  border:2px solid blue;
-  & > ul{
-    display: flex;
-    >li{
-      width:33.33%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
- 
-`;
+
 
 function App() {
   return (
@@ -53,19 +41,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav />
       </Wrapper>
     </Router>
   );
