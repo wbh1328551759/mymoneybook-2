3a9dba9f3f05f0a794cc00b0984f6512cf5cccdf
wbh1328551759 <1328551759@qq.com>
2020-07-03 00:27:42 +0800

    Money.tsx UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 const TagsSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   >ol{
     >li{
       background: #D9D9D9;
@@ -117,10 +122,14 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`;
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -165,8 +174,7 @@ function Money() {
           <button className="dot">.</button>
         </div>
       </NumberPadSection>
-
-    </Layout>
+    </MyLayout>
   );
 }
 
