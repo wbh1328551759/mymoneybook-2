103b774913e1be8762581ce367bc28cd773a6d0f
wbh1328551759 <1328551759@qq.com>
2020-07-05 21:51:23 +0800

    编辑 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -9,12 +9,30 @@ const defaultTags = [
 ];
 
 const useTags = () => { //封装一个自定义 Hook
-  const [tags, setTags] = useState<{id: number, name: string}[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const finTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    const index = findTag(id);
+    const tagClone = JSON.parse(JSON.stringify(tags));
+    tagClone.splice(index, 1, {id: id, name: obj.name});
+    setTags(tagClone);
+  };
   return {
     tags: tags,
     setTags: setTags,
-    findTag: findTag
+    findTag: findTag,
+    updateTag: updateTag,
+    finTagIndex: finTagIndex
   };
 };
 
